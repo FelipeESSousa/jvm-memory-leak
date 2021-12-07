b862aa5c0da4a604e1d4fca89216e858b108ac3d
Felipe Sousa - Ventania <felipe.eduardo@tqi.com.br>
2021-12-07 16:49:03 -0300

    Primeiro commit

---
+++ src/main/java/br/com/memoryleak/MemoryleakApplication.java
@@ -0,0 +1,13 @@
+package br.com.memoryleak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryleakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryleakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/memoryleak/MemoryleakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.memoryleak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryleakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
